b132d22d41c95f53ad4bd91ec44d9186caad576d
Jerrell Prier <jerrellprier@gmail.com>
2021-11-11 14:58:25 -0600

    Changed file by using .seek method to start each loop at top of file instead of closing and reopeing file.

--- data_presenter.py
+++ data_presenter.py
@@ -5,23 +5,23 @@ open_file = open("CupcakeInvoices.csv")
 
 for cupcake in open_file:
     print(cupcake)
-
-open_file.close()
+open_file.seek(0)
+#open_file.close()
 
 print("\nPrinting each cupcake type:\n")
-csv_file = open("CupcakeInvoices.csv")
-for type in csv.reader(csv_file):
+#csv_file = open("CupcakeInvoices.csv")
+for type in csv.reader(open_file):
     print(type[2])
-
-csv_file.close()
+open_file.seek(0)
+#csv_file.close()
 total = 0
 
 print("\nTotal for each invoice:\n")
-csv2_file = open("CupcakeInvoices.csv")
-for sum in csv.reader(csv2_file):
+#csv2_file = open("CupcakeInvoices.csv")
+for sum in csv.reader(open_file):
     print(round(int(sum[3])*float(sum[4]),2))
     total += int(sum[3])*float(sum[4])
 
-csv2_file.close()
+#csv2_file.close()
 
 print("The combined total is: ", round(total,2))
